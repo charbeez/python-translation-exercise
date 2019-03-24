ed66601ed64084fd371f595db85618548f6f5afd
Charlotte Benedict <charbeezy12@gmail.com>
2019-03-24 10:40:13 -0500

    final changes

--- translate.py
+++ translate.py
@@ -18,63 +18,66 @@ def translate_sequence(rna_sequence, genetic_code):
     return "".join(amino_acid_list)
 
 def get_all_translations(rna_sequence, genetic_code):
-    """Get a list of all amino acid sequences encoded by an RNA sequence.
-
-    All three reading frames of `rna_sequence` are scanned from 'left' to
-    'right', and the generation of a sequence of amino acids is started
-    whenever the start codon 'AUG' is found. The `rna_sequence` is assumed to
-    be in the correct orientation (i.e., no reverse and/or complement of the
-    sequence is explored).
-
-    The function returns a list of all possible amino acid sequences that
-    are encoded by `rna_sequence`.
-
-    If no amino acids can be translated from `rna_sequence`, an empty list is
-    returned.
-    """
-    pass
+    rna_sequence = rna_sequence.upper()
+    number_of_bases = len(rna_sequence)
+    last_codon_index = number_of_bases - 3
+    if last_codon_index < 0:
+        return ('')
+    amino_acid_seq_list = []
+    for base_index in range(last_codon_index +1):
+        codon = rna_sequence[base_index: base_index +3]
+        if codon == "AUG":
+            aa_seq = translate_sequence(
+            rna_sequence = rna_sequence[base_index:],
+            genetic_code = genetic_code)
+            if aa_seq:
+                amino_acid_seq_list.append(aa_seq)
+    return amino_acid_seq_list
 
 def get_reverse(sequence):
-    """Reverse orientation of `sequence`.
-
-    Returns a string with `sequence` in the reverse order.
-
-    If `sequence` is empty, an empty string is returned.
-    """
-    pass
+    if sequence:
+        seq=sequence.upper()
+        reverse_seq=seq[::-1]
+        return reverse_seq
+    else:
+        return ''
 
 def get_complement(sequence):
-    """Get the complement of `sequence`.
+    if sequence:
+        seq=list(sequence.upper())
+        complements = {'C':'G','G':'C','U':'A','A':'U'}
+        seq=[complements[base] for base in seq]
+        return ''.join(seq)
+    else:
+        return ''    
 
-    Returns a string with the complementary sequence of `sequence`.
-
-    If `sequence` is empty, an empty string is returned.
-    """
-    pass
 
 def reverse_and_complement(sequence):
-    """Get the reversed and complemented form of `sequence`.
-
-    Returns a string that is the reversed and complemented sequence
-    of `sequence`.
-
-    If `sequence` is empty, an empty string is returned.
-    """
-    pass
+    if sequence:
+        seq = get_reverse(sequence)
+        rev_comp = get_complement(seq)
+        return rev_comp
+    else:
+        return ''
 
 def get_longest_peptide(rna_sequence, genetic_code):
-    """Get the longest peptide encoded by an RNA sequence.
-
-    Explore six reading frames of `rna_sequence` (three reading frames of the
-    current orientation, and the reversed and complemented form) and return (as
-    a string) the longest sequence of amino acids that it encodes, according to
-    the `genetic_code`.
-
-    If no amino acids can be translated from `rna_sequence` nor its reverse and
-    complement, an empty list is returned.
-    """
-    pass
-
+    peptides = get_all_translations(rna_sequence,
+        genetic_code = genetic_code)
+    rev_comp_seq = reverse_and_complement(rna_sequence)
+    rev_comp_peptides = get_all_translations(rna_sequence = rev_comp_seq,
+        genetic_code = genetic_code)
+    peptides += rev_comp_peptides
+    if not peptides:
+        return ""
+    if len(peptides) <2:
+        return peptides [0]
+    most_number_of_bases = -1
+    longest_peptide_index = -1
+    for peptide_index, aa_seq in enumerate(peptides):
+        if len(aa_seq) > most_number_of_bases:
+            longest_peptide_index = peptide_index
+            most_number_of_bases = len(aa_seq)
+    return peptides[longest_peptide_index]
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
